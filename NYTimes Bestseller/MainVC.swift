e8955fb727196a1b82ace551c3bada568ef850bb
Lingeswaran Kandasamy <lingeswarankandasamy@Lingeswarans-iMac.local>
2018-01-03 18:17:37 -0500

    Completed data model creation and main UI design

---
+++ NYTimes Bestseller/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NYTimes Bestseller
+//
+//  Created by Lingeswaran Kandasamy on 1/3/18.
+//  Copyright © 2018 Lingeswaran Kandasamy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
