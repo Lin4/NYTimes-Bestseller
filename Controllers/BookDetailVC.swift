287c78fd9ee96e3402423126aaa164c622171ca9
Lingeswaran Kandasamy <lingeswarankandasamy@Lingeswarans-iMac.local>
2018-01-05 17:43:53 -0500

    collection view sort completed

--- Controllers/BookDetailVC.swift
+++ Controllers/BookDetailVC.swift
@@ -13,9 +13,13 @@ import SwiftyJSON
 class BookDetailVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource
 {
 
-    
     @IBOutlet weak var bookCollection: UICollectionView!
+     @IBOutlet weak var segment: UISegmentedControl!
+    
     var bestSellerList: String!
+    var dataSort: String! = "by_week"
+    var sortedBookArray: [books]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         bookCollection.dataSource = self
@@ -34,14 +38,16 @@ class BookDetailVC: UIViewController, UICollectionViewDelegate, UICollectionView
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
             return BookDetailService.instance.book.count
+       
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookCell", for: indexPath) as? BookCell {
-            let books = BookDetailService.instance.book[indexPath.row]
-            cell.updateViews(book: books)
-            return cell
-        }
+                sortBookArray(dataSort: dataSort)
+                let books = sortedBookArray[indexPath.row]
+                cell.updateViews(book: books)
+                return cell
+            }
         return BookCell()
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -51,4 +57,35 @@ class BookDetailVC: UIViewController, UICollectionViewDelegate, UICollectionView
         self.navigationController?.pushViewController(bookVC, animated: true)
         bookVC.selectedBooks = book
     }
+    
+    func attemptFetch() {
+      if segment.selectedSegmentIndex == 0 {
+            dataSort = "by_week"
+        } else if segment.selectedSegmentIndex == 1 {
+        
+            dataSort = "rank"
+        }
+    }
+    
+    func sortBookArray(dataSort: String) {
+        if dataSort == "by_week"{
+         let arraySory = BookDetailService.instance.book
+            sortedBookArray = arraySory.sorted {
+            $0.weeksOnList > $1.weeksOnList
+            }
+        }else if dataSort == "rank"{
+            let arraySory = BookDetailService.instance.book
+            sortedBookArray = arraySory.sorted {
+                $0.rank < $1.rank
+            }
+    }
+}
+    @IBAction func segmentChange(_ sender: AnyObject) {
+        attemptFetch()
+        print("LIN", dataSort)
+        sortBookArray(dataSort: dataSort)
+        bookCollection.reloadData()
+        
+    }
+    
 }
--- Utilities/Constants.swift
+++ Utilities/Constants.swift
@@ -14,5 +14,3 @@ var param = BestSellerListName.instance.bestSellerListName.replacingOccurrences(
 let BASE_URL = "https://api.nytimes.com/svc/books/v3/"
 let URL_GET_BEST_SELLERS_LIST = "\(BASE_URL)lists/names.json?api-key=92c2940599c54dadb9bd2517a8d82226"
 let URL_GET_BEST_SELLERS_LIST_DETAILS = "\(BASE_URL)lists/2016-12-11/\(param).json?api-key=92c2940599c54dadb9bd2517a8d82226"
-//let URL_GET_BEST_SELLERS_LIST_DETAILS = "\(BASE_URL)lists/2016-12-11/mass-market-paperback.json?api-key=92c2940599c54dadb9bd2517a8d82226"
-
